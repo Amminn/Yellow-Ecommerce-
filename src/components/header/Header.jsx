df45897054eea5c43be9831b126f4a1be4486fdf
Amminn <vrb.nnn@gmail.com>
2023-01-05 00:09:27 +0100

    init: initial files

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from './Header.module.css'
+import Logo from '../../assets/logo.png'
+import {CgShoppingBag} from 'react-icons/cg'
+
+function Header() {
+  return (
+    <p>header</p>
+  )
+}
+
+export default Header
